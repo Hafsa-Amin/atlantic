cd035950b967395a02bff4fe2803d83ab2923ce8
Hafsa Amin <hafsaamin122@gmail.com>
2021-05-16 04:34:25 -0700

    Admin Product Update

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -15,6 +15,7 @@ import ProfileScreen from './screens/ProfileScreen';
 import PrivateRoute from './components/PrivateRoute';
 import AdminRoute from './components/AdminRoute';
 import ProductListScreen from './screens/ProductListScreen';
+import ProductEditScreen from './screens/ProductEditScreen';
 
 function App() {
   const cart = useSelector((state) => state.cart);
@@ -30,59 +31,59 @@ function App() {
 
   return (
     <BrowserRouter>
-      <div className="grid-container">
-        <header className="row">
+      <div className='grid-container'>
+        <header className='row'>
           <div>
-            <Link className="brand" to="/">
+            <Link className='brand' to='/'>
               amazon.com
             </Link>
           </div>
           <div>
-            <Link to="/cart">
+            <Link to='/cart'>
               Cart
               {cartItems.length > 0 && (
-                <span className="badge">{cartItems.length}</span>
+                <span className='badge'>{cartItems.length}</span>
               )}
             </Link>
             {userInfo ? (
-              <div className="dropdown">
-                <Link to="#">
-                  {userInfo.name} <i className="fa fa-caret-down"></i>
+              <div className='dropdown'>
+                <Link to='#'>
+                  {userInfo.name} <i className='fa fa-caret-down'></i>
                 </Link>
-                <ul className="dropdown-content">
+                <ul className='dropdown-content'>
                   <li>
-                    <Link to="/profile">User Profile</Link>
+                    <Link to='/profile'>User Profile</Link>
                   </li>
                   <li>
-                    <Link to="/orderhistory">Order History</Link>
+                    <Link to='/orderhistory'>Order History</Link>
                   </li>
                   <li>
-                    <Link to="#signout" onClick={signoutHandler}>
+                    <Link to='#signout' onClick={signoutHandler}>
                       Sign Out
                     </Link>
                   </li>
                 </ul>
               </div>
             ) : (
-              <Link to="/signin">Sign In</Link>
+              <Link to='/signin'>Sign In</Link>
             )}
             {userInfo && userInfo.isAdmin && (
-              <div className="dropdown">
-                <Link to="#admin">
-                  Admin <i className="fa fa-caret-down"></i>
+              <div className='dropdown'>
+                <Link to='#admin'>
+                  Admin <i className='fa fa-caret-down'></i>
                 </Link>
-                <ul className="dropdown-content">
+                <ul className='dropdown-content'>
                   <li>
-                    <Link to="/dashboard">Dashboard</Link>
+                    <Link to='/dashboard'>Dashboard</Link>
                   </li>
                   <li>
-                    <Link to="/productlist">Products</Link>
+                    <Link to='/productlist'>Products</Link>
                   </li>
                   <li>
-                    <Link to="/orderlist">Orders</Link>
+                    <Link to='/orderlist'>Orders</Link>
                   </li>
                   <li>
-                    <Link to="/userlist">Users</Link>
+                    <Link to='/userlist'>Users</Link>
                   </li>
                 </ul>
               </div>
@@ -90,26 +91,31 @@ function App() {
           </div>
         </header>
         <main>
-          <Route path="/" component={HomeScreen} exact></Route>
-          <Route path="/register" component={RegisterScreen}></Route>
-          <Route path="/signin" component={SigninScreen}></Route>
-          <Route path="/product/:id" component={ProductScreen} exact></Route>
-          <Route path="/cart/:id?" component={CartScreen}></Route>
-          <Route path="/shipping" component={ShippingAddressScreen}></Route>
-          <Route path="/payment" component={PaymentMethodScreen}></Route>
-          <Route path="/placeorder" component={PlaceOrderScreen}></Route>
-          <Route path="/order/:id" component={OrderScreen}></Route>
-          <Route path="/orderhistory" component={OrderHistoryScreen}></Route>
+          <Route path='/' component={HomeScreen} exact></Route>
+          <Route path='/register' component={RegisterScreen}></Route>
+          <Route path='/signin' component={SigninScreen}></Route>
+          <Route path='/product/:id' component={ProductScreen} exact></Route>
+          <Route
+            path='/product/:id/edit'
+            component={ProductEditScreen}
+            exact
+          ></Route>
+          <Route path='/cart/:id?' component={CartScreen}></Route>
+          <Route path='/shipping' component={ShippingAddressScreen}></Route>
+          <Route path='/payment' component={PaymentMethodScreen}></Route>
+          <Route path='/placeorder' component={PlaceOrderScreen}></Route>
+          <Route path='/order/:id' component={OrderScreen}></Route>
+          <Route path='/orderhistory' component={OrderHistoryScreen}></Route>
           <PrivateRoute
-            path="/profile"
+            path='/profile'
             component={ProfileScreen}
           ></PrivateRoute>
           <AdminRoute
-            path="/productlist"
+            path='/productlist'
             component={ProductListScreen}
           ></AdminRoute>
         </main>
-        <footer className="row center">All right reserved</footer>
+        <footer className='row center'>All right reserved</footer>
       </div>
     </BrowserRouter>
   );
